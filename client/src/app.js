ea20958b4fe7d7f7eb9911fc4c0b1ac6397ab648
Afonso Drumond <afonso.sousa.martins@gmail.com>
2024-05-11 19:46:49 +0100

    new

--- client/src/app.js
+++ client/src/app.js
@@ -3,12 +3,13 @@ import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import Home from './home';
 import Blog from './components/blog';
 import Login from './components/login';
-import Error from './components/error';
 import Precos from './components/precos';
 import Parceiro from "./components/parceiros";
 import Register from "./components/registro";
 import Password from "./components/password";
 import ResetPassword from "./components/resetpassword";
+import Error from "./components/error";
+import Dashboard from "./components/dashboard";
 
 
 
@@ -29,6 +30,8 @@ function App() {
       <Route path='/parceiros' element={<Parceiro />} />
       <Route path='/registro' element={<Register />} />
       <Route path='/password' element={<Password />} />
+      <Route path='/error' element={<Error />} />
+      <Route path='/dashboard' element={<Dashboard />} />
       <Route path="/reset-password/:token" element={<ResetPassword/>} />
     </Routes>
   );
