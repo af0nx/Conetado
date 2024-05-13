d07d97c34f564c6767752fb6876d20e12bb05fa8
Afonso Drumond <afonso.sousa.martins@gmail.com>
2024-05-13 23:54:55 +0100

    New

--- README.md
+++ README.md
@@ -9,3 +9,13 @@ npx nodemon index.js
 A MongoDB foi realizada a partir do nosso email, é necessario whitelist, preciso dos ips para colocar lá. Para posteriormente ver o dashboard e etc.
 
 Para instalar dependecias ver no terminal de cada um e instalar fora de ambas
+
+
+Creditos:
+
+https://www.youtube.com/watch?v=Lag9Pj_33hM&list=LL&index=2
+https://merakiui.com/components/marketing/blog
+https://tailwindcomponents.com/component/page-sections-2
+https://replicate.com/stability-ai/sdxl
+https://tailwindcomponents.com/component/payment-setup-1
+https://tailwindflex.com/@alok/feature-showcase
--- client/src/components/others/dashboard.jsx
+++ client/src/components/others/dashboard.jsx
@@ -111,14 +111,7 @@ const Dashboards = () => {
                   Promoções
                 </a>
               </li>
-              <li>
-                <a
-                  href="/dashboard/perfil"
-                  className="block py-2 px-3 text-gray-50  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Perfil
-                </a>
-              </li>
+
 
               <button onClick={handleLogout} className="text-white">
                 Logout
